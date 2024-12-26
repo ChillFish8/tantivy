8b4cf6033b14c4c70624aca1fbb1aa4c3d5fd16d
chillfish8 <hburt2003@gmail.com>
2024-12-26 13:04:19 +0000

    Update docs on exposed methods

--- src/indexer/operation.rs
+++ src/indexer/operation.rs
@@ -1,3 +1,8 @@
+//! Internal operations for working with single segments
+//!
+//! WARNING: This is an advanced module, and you shouldn't use the things in here
+//! unless you absolutely know what you're doing.
+
 use crate::query::Weight;
 use crate::schema::document::Document;
 use crate::schema::{TantivyDocument, Term};
@@ -5,14 +10,18 @@ use crate::Opstamp;
 
 /// Timestamped Delete operation.
 pub struct DeleteOperation {
+    /// The [Opstamp] assigned to this operation.
     pub opstamp: Opstamp,
+    /// The weight used for selecting the documents to delete.
     pub target: Box<dyn Weight>,
 }
 
 /// Timestamped Add operation.
 #[derive(Eq, PartialEq, Debug)]
 pub struct AddOperation<D: Document = TantivyDocument> {
+    /// The [Opstamp] assigned to this operation.
     pub opstamp: Opstamp,
+    /// The document data to be indexed.
     pub document: D,
 }
 
